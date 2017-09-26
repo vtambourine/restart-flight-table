c76383a03504d8c0777e4ffca8c743294ff7ad3d
vtambourine <vtambourine@yandex.ru>
2017-09-26 14:52:57 +0000

    FlightAPI

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* global $ */
 class Flight {
   constructor(flight) {
     this.name = flight.flightName;
@@ -9,7 +10,9 @@ class Flight {
     this.destinations = flight.route.destinations.toString();
     this.gate = flight.gate;
     this.terminal = flight.terminal;
-    this.status = flight.publicFlightState.flightStates.toString();
+    this.status = flight.publicFlightState.flightStates
+      .map(state => DEPARTING_STATUSES[state])
+      .toString();
 
     this.element = null;
   }
@@ -29,7 +32,6 @@ class Flight {
     */
     this.element = $(`
       <tr>
-        <td>${this.terminal}</td>
         <td>${this.number}</td>
         <td>${this.destinations}</td>
         <td>${this.time}</td>
@@ -96,6 +98,86 @@ const ARRIVING_STATUSES = {
   TOM: 'Tomorrow',
 };
 
+const FLIGHT_DIRECTIONS = {
+  DEPARTING: 'D',
+  ARRIVING: 'A'
+};
+
+
+class FlightApi {
+  constructor({appId, appKey}) {
+    this.appId = appId;
+    this.appKey = appKey;
+    this.destinationsCache = {};
+  }
+  
+  // Direction: 'A' for arrival, 'D' for departure
+  getFlights(direction, success, error) {
+
+    $.ajax({
+      url: 'https://api.schiphol.nl/public-flights/flights',
+      headers: {
+        'ResourceVersion': 'v3',
+      },
+      dataType: 'json',
+      data: {
+        app_id: this.appId,
+        app_key: this.appKey,
+        flightdirection: direction,
+        includedelays: true,
+        scheduletime: '12:00',
+        sort: '+scheduletime',
+        page: '0',
+      },
+      context: this,
+      success: success,
+      error: error
+    });
+  
+  }
+  
+  getDestination(destinationCode, success, error) {
+    
+    // By caching the response we avoid calling the API multiple
+    // times
+    if (this.destinationsCache[destinationCode] != undefined ){
+      success(this.destinationsCache[destinationCode]);
+    } 
+    
+    let api = this;
+    // get destinations here.
+    $.ajax({
+      url: 'https://api.schiphol.nl/public-flights/destinations/'+destinationCode,
+      headers: {
+        'ResourceVersion': 'v1',
+      },
+      dataType: 'json',
+      data: {
+        app_id: api.appId,
+        app_key: api.appKey,
+      },
+      success: function(data, status, xhr) {
+        console.log(xhr.getResponseHeader('Link'));
+        console.log(data);
+        
+        let destination = {
+          'city': data.city,
+          'country': data.country
+        };
+        
+        api.destinationsCache[destinationCode] = destination;
+
+        success(destination);
+
+      },
+      error: function () {
+        error();
+      }
+    });
+  }
+}
+
+
 (function () {
 
   const flightTable = new FlightTable([
@@ -106,6 +188,29 @@ const ARRIVING_STATUSES = {
 
   flightTable.render(document.getElementById('flight-table'));
 
+  const api = new FlightApi({
+    appId: window.B.app_id,
+    appKey: window.B.app_key
+  });
+  
+  function getFlights() {
+    api.getFlights(
+      FLIGHT_DIRECTIONS.DEPARTING,
+      (data) => { 
+        data.flights.forEach(function (flight) {
+          flightTable.update(flight);
+        });
+      }, 
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  
+  $('#refresh').click(getFlights);
+  
+  getFlights();
+/*
   $.ajax({
     url: 'https://api.schiphol.nl/public-flights/flights',
     headers: {
@@ -138,7 +243,7 @@ const ARRIVING_STATUSES = {
     error: function () {
       console.error('Unable to fetch flight details. See Network tab');
     }
-  });
+  });*/
 })();
 
 
