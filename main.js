490c57939ee62428bdc73a7167d340a36102100e
Benjamin T <vtambourine@ya.ru>
2017-09-26 15:18:35 +0200

    API Draft

--- main.js
+++ main.js
@@ -1,5 +1,66 @@
+class Flight {
+  constructor(flight) {
+    this.name = flight.name;
+    this.date = flight.scheduleDate;
+    this.time = flight.scheduleTime;
+    this.datetime = Date.parse(flight.actualOffBlockTime);
+    this.formattedString = this.datetime.toLocaleString();
+
+    this.element = null;
+  }
+
+  update(flightDetails) {
+    console.log('fligth updated:', flightDetails.flightName);
+
+    this.element.classList.add('updated');
+  }
+
+  render() {
+    this.element = $(`
+      <tr>
+        <td>${this.time}</td>
+        <td>${this.name}</td>
+        <td>${'dest'}</td>
+      </tr>
+    `);
+    return this.element;
+  }
+
+}
+
+class FlightTable {
+  constructor(headers) {
+    this.headers = headers;
+    this.flights = {};
+  }
+
+  render(tableElement) {
+    if (!(tableElement instanceof HTMLTableElement)) {
+      throw new Error('FlightTable#render: tableElement must be a <table>');
+    }
+    this.element = tableElement;
+    this.tableHeader = tableElement.querySelector('thead');
+  }
+
+  update(flightDetails) {
+    let flight = this.flights[flightDetails.flightName];
+    if (!flight) {
+      flight = this.flights[flightDetails.flightName] = new Flight(flightDetails);
+    }
+
+    flight.update(flightDetails);
+  }
+}
+
 (function () {
-  console.log('loaded');
+
+  const flightTable = new FlightTable([
+    'Time',
+    'Number',
+    'Destination'
+  ]);
+
+  flightTable.render(document.getElementById('flight-table'));
 
   $.ajax({
     url: 'https://api.schiphol.nl/public-flights/flights',
@@ -10,13 +71,32 @@
     data: {
       app_id: window.B.app_id,
       app_key: window.B.app_key,
-      flightdirection: 'A',
-      fromdate: '2017-09-18',
-      todate: '2017-09-19'
+      flightdirection: 'D',
+      includedelays: true,
+      // scheduledate: '2017-09-21',
+      // scheduletime: '12:00',
+      sort: '+scheduletime',
+      page: '0',
     },
-    success: function(data) {
-      console.log('hey!!!');
+    success: function(data, status, xhr) {
+      console.log(xhr.getResponseHeader('Link'))
       console.log(data)
+      let output = [];
+      data.flights.forEach(function (flight) {
+        output.push({
+          flightName: flight.flightName,
+          scheduleDate: flight.scheduleDate,
+          scheduleTime: flight.scheduleTime,
+          actualOffBlockTime: flight.actualOffBlockTime
+        });
+
+        flightTable.update(flight);
+      });
+      // console.table(output);
+    },
+    error: function () {
+      console.error('Unable to fetch flight details. See Network tab');
+      // console.error.call(this, Array.prototype.slice.call(arguments));
     }
   });
 })();
